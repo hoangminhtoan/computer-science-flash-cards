222f45cc7c3e84e3fd747078bf28916d4c8dbde6
hoangminhtoan <hoangminhtoant2l1@yahoo.com>
2020-08-07 14:27:28 +0900

    Update Code

--- install_env.sh
+++ install_env.sh
@@ -5,18 +5,28 @@ sudo apt-get install -y sqlite3
 sudo apt install libcurl4-openssl-dev libssl-dev
 
 # Working on virtual environment
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0  <Virtual Env>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0  <method> <Virtual Env>"
     exit
 fi
 
-env_name="$1"
+method="$1"
+env_name="$2"
 
-echo "Activate virtual environment"
+echo ""
 
-source `which virtualenvwrapper.sh`
-mkvirtualenv $env_name -p python3
-source $HOME/.virtualenvs/$env_name/bin/activate
-workon $env_name
+if [ $method == "pip" ];then
+    echo "Activate virtual environment by using virtualwrapenv"
+    source `which virtualenvwrapper.sh`
+    mkvirtualenv $env_name -p python3
+    source $HOME/.virtualenvs/$env_name/bin/activate
+    workon $env_name
+elif [ $method == "conda"];then
+    echo "Activate virtual environment by using conda"
+    conda create -n $env_name python=3.6
+    conda activate $env_name
+else 
+    echo "Insert pip or conda in order to create virtual environment"
+fi
 
 pip install -r requirements.txt
