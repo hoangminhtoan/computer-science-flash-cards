dd142784c0bbb395f717881bbee86325ad33fa67
hoangminhtoan <hoangminhtoant2l1@yahoo.com>
2020-08-07 13:28:50 +0900

    Update Code

---
+++ install_env.sh
@@ -0,0 +1,19 @@
+# run this command before install pycurl
+sudo apt install libcurl4-openssl-dev libssl-dev
+
+# Working on virtual environment
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0  <Virtual Env>"
+    exit
+fi
+
+env_name="$1"
+
+echo "Activate virtual environment"
+
+source `which virtualenvwrapper.sh`
+mkvirtualenv $env_name -p python3
+source $HOME/.virtualenvs/$env_name/bin/activate
+workon $env_name
+
+pip install -r requirements.txt
